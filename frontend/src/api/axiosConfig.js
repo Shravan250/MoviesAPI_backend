2ac2a59187d4958db1a8d7b7e0b571baf2a662e5
Shravan250 <shravanbobade25@gmail.com>
2024-12-19 20:43:49 +0530

    ready for deployment

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://beee-2409-40c2-11a-814f-8104-ac34-6277-39a2.ngrok-free.app", // Updated ngrok URL
+  baseURL: "http://localhost:8080",
   headers: { "ngrok-skip-browser-warning": "true" },
 });
